236d9520db9a97b46cb73e95ed49afd3954c0274
AKIB2005 <71378420+AKIB2005@users.noreply.github.com>
2020-09-28 14:43:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedRect, movingRect;
+var gameObject1,gameObject2,gameObject3,gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 20, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  gameObject1=createSprite(100,100,50,50);
+  gameObject2=createSprite(200,100,50,50);
+  gameObject3=createSprite(300,100,50,50);
+  gameObject4=createSprite(600,780,50,50);
+  gameObject1.shapeColor="green";
+  gameObject2.shapeColor="green";
+  gameObject3.shapeColor="green";
+  gameObject4.shapeColor="green";
+  fixedRect.velocityY=5;
+  gameObject4.velocityY=-5;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+bounceOff (fixedRect,gameObject4);
+ isTouching (movingRect,gameObject1); 
+  drawSprites();
+}
+
